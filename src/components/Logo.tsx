da52509e47d73e81a0bca959f933c6914ad1d20d
naveenchin <naveenchintala1228@gmail.com>
2025-11-30 23:51:35 +0100

    Updated index.html

--- src/components/Logo.tsx
+++ src/components/Logo.tsx
@@ -7,19 +7,12 @@ interface LogoProps {
 
 export const Logo: React.FC<LogoProps> = ({ className = "", size = 40 }) => {
   return (
-    <svg
+    <img
+      src="/logo_1024.png"
+      alt="TrustTrack Logo"
       width={size}
       height={size}
-      viewBox="0 0 40 40"
-      fill="none"
-      xmlns="http://www.w3.org/2000/svg"
       className={className}
-    >
-      <rect width="40" height="40" rx="8" fill="#3B82F6"/>
-      <path
-        d="M12 12h16v4h-6v12h-4V16h-6v-4z"
-        fill="white"
-      />
-    </svg>
+    />
   );
 };
